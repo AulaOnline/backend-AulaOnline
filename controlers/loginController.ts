f86a755cf793507874c91cb8594481fb18321d88
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-02-23 01:39:38 -0300

    First Commit

--- controlers/loginController.ts
+++ controlers/loginController.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('fodase os usuários');
+  res.send('fodase todos usuários');
 });
 
 export default router;
