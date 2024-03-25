c6c1bcf63fec01868f3d9663bd3c2b0d32b1882a
gabitenoriob <gabitenoriob@gmail.com>
2024-03-25 18:01:02 -0300

    testando deploy

--- app.ts
+++ app.ts
@@ -1,68 +1,58 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();
-const PORT = process.env.PORT
-const HOSTNAME = process.env.DB_HOST
-const app = express()
+import { DataSource } from 'typeorm';
+import { User } from "./core/entities/userEntitie";
+import { Video } from './core/entities/videoEntitie';
+import { Annotation } from './core/entities/AnnotationEntitie';
+import { Summary } from "./core/entities/summaryEntitie";
 import customResponse from './core/model/customResponse';
 import loginController from './features/login/loginController';
 import videoController from './features/video/VideoController';
 import aiController from "./features/AIgenerations/aiController";
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { User } from "./core/entities/userEntitie";
-import { Video } from './core/entities/videoEntitie';
-import { Annotation } from './core/entities/AnnotationEntitie';
-import {Summary} from "./core/entities/summaryEntitie";
-const cookieParser = require('cookie-parser');
 import annotationController from "./features/annotation/annotationController";
+const cookieParser = require('cookie-parser');
+
 dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT;
+const HOSTNAME = process.env.DB_HOST;
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbName = process.env.DB_DBNAME;
-
+const DB_CONNECTION_URI = process.env.DB_CONNECTION_URI;
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    database: dbName,
-    username: dbUser,
-    password: dbPassword,
-    //1º uso mudar para true:
+    url: DB_CONNECTION_URI, 
     synchronize: true,
     entities: [User, Video, Annotation, Summary]
-})
+});
 
 AppDataSource.initialize()
     .then(() => {
-        console.log("Conexao estabelecida");
+        console.log("Conexão estabelecida");
     })
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send('Hello World!') })
+app.get('/', (req, res) => { res.send('Hello World!') });
 
 app.use(cors({
     origin: ['https://aulaonline.onrender.com']
-}))
+}));
 
-//Adcione suas rotas, senão apesar de criadas não vão ser usadas...
 app.use('/login', loginController);
 app.use('/video', videoController);
 app.use('/annotation', annotationController);
-app.use('/generate', aiController)
-
+app.use('/generate', aiController);
 
 app.use((req, res) => {
     return res.status(404).json(new customResponse(404, "Erro 404 Detectado", null));
-})
+});
+
 app.listen(PORT, () => {
-    console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`)
-})
-
-
+    console.log(`Servidor rodando com sucesso ${HOSTNAME}:${PORT}`);
+});
